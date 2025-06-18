f186c381bdd45d46099e425c41087ae35f6bac24
VengammaV <vengammav@gmail.com>
2025-06-18 11:38:06 +0530

    updated code

--- app1.py
+++ app1.py
@@ -6,7 +6,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 import seaborn as sns
 import matplotlib.pyplot as plt
 import spacy
-import en_core_web_sm
 from sentence_transformers import SentenceTransformer
 
 # ----------------------
@@ -118,7 +117,12 @@ def hybrid_recommender(book_title, top_n=5):
     return df.loc[content_idx], df.iloc[hybrid_indices][['book_name', 'author', 'genre', 'rating']]
 
 # load english language model and create nlp object from it
-nlp = spacy.load("en_core_web_sm") 
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 #use this utility function to get the preprocessed text data
 def preprocess(text):
     if pd.isna(text):  # Handles NaN or None safely
